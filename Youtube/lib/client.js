8b001e508be45ada3a7c74372eda3b0187447886
exlted <exltedofdrg@gmail.com>
2022-07-03 21:36:13 -0500

    Clean up code in prep for Dockerization

--- Youtube/lib/client.js
+++ Youtube/lib/client.js
@@ -2,7 +2,6 @@ const { EventEmitter } = require('events');
 const { inherits } = require('util');
 const axios = require('axios');
 const Utils = require('./utils')
-const Parser = require('./parser')
 
 
 function YoutubeChat({ channelId }) {
