72856cfe0b43df6b89108d6cce006fac2402262e
exlted <exltedofdrg@gmail.com>
2022-08-17 18:11:43 -0500

    #35 Undo a thing that broke emotes more

--- Youtube/lib/pageScraper.js
+++ Youtube/lib/pageScraper.js
@@ -55,7 +55,7 @@ const chatScraper = {
                                             }
 
                                             emotes.push({
-                                                urls: [url],
+                                                url: url,
                                                 name: name
                                             });
                                         }
