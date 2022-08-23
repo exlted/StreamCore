2e9a9037ef93e289fa913d855fa3a4eb0d896c90
exlted <exltedofdrg@gmail.com>
2022-08-22 21:34:52 -0500

    #35 Fix all YT emotes this time

--- Youtube/lib/pageScraper.js
+++ Youtube/lib/pageScraper.js
@@ -61,7 +61,7 @@ const chatScraper = {
                                         }
                                     }
                                     let rawHTML = message.innerHTML;
-                                    let cleaned_msg = rawHTML.replace(/<.*?shared-tooltip-text="(.*?)".*?>/gm, "$1 ");
+                                    let cleaned_msg = rawHTML.replace(/<.*?shared-tooltip-text="(.*?)".*?>/gm, " $1 ");
                                     cleaned_msg = cleaned_msg.replace(/<.*?>/gm, "");
 
                                     let data = {
